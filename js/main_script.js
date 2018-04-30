8bdc0ed569dde34f7b8e03feab74175428e8bd02
DariaDimchuk <daria.dimchuk@gmail.com>
2018-04-30 11:44:40 -0700

    Formatted and commented

--- js/main_script.js
+++ js/main_script.js
@@ -1,31 +1,37 @@
 /*	SEARCH FUNCTIONALITY	*/
 
 
-/*	Store search value in variable	*/
+	/*	Store the search input in a variable 	*/
+	function getSearchValue() {
+		var searchInput = document.getElementById("searchbar").value;
+		
+		return searchInput;
+	}
+
+	/*	Get the search result from the URL	*/
+	function getURLsearchValue(name, url) {
+		if (!url) url = window.location.href;
+		name = name.replace(/[\[\]]/g, "\\$&");
+		var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+			results = regex.exec(url);
+		if (!results) return null;
+		if (!results[2]) return '';
+		return decodeURIComponent(results[2].replace(/\+/g, " "));
+	}
 
 
-function getURLsearchValue(name, url) {
-	if (!url) url = window.location.href;
-	name = name.replace(/[\[\]]/g, "\\$&");
-	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-		results = regex.exec(url);
-	if (!results) return null;
-	if (!results[2]) return '';
-	return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
+/*	WINDOW ONLOAD FUNCTIONALITY		*/
 
 
-function getSearchValue() {
-	var searchInput = document.getElementById("searchbar").value;
-	
-	return searchInput;
-}
-
-
-/*	Trigger search when enter is hit	*/
-
-	//must be made onload - otherwise "searchbar" returns null value
 	window.onload=function(){
+		
+		/*		
+		*
+		Trigger search when enter is hit	
+			- must be made onload - otherwise "searchbar" returns null value
+		*
+		*/
+		
 		//get value that is being searched
 		var input = document.getElementById("searchbar");
 		
@@ -36,16 +42,15 @@ function getSearchValue() {
 				document.getElementById("search-button").click();
 			}
 		});
-	}
-	
-	
-	
-/*	Search button functionality	*/
+		
+	}//end window.onload
+
+
+
+	/*	Search button functionality	*/
 
 	//load search results website on button click
 	function search(){
-		
-		//onclick=window.location.href = './page_2.html';
 		onclick=window.location.href = './page_2.html?query=' + getSearchValue();;
 	}
-	
+
