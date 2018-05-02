3370af8bdb41da2b6909cf19027c110dd1125890
DariaDimchuk <daria.dimchuk@gmail.com>
2018-05-01 22:31:39 -0700

    Patch: search menu function

--- js/main_script.js
+++ js/main_script.js
@@ -1,3 +1,54 @@
+
+	$(document).on('keyup', function(e) {
+		if (e.target.id === 'searchbar') {
+			if(e.target.value !== ""){
+				$('#search-suggestions').show();
+			}else{
+				$('#search-suggestions').hide();
+			}			
+		}
+	});
+
+	$(document).on('click', function(e) {
+		if (e.target.id !== 'searchbar') {
+			$('#search-suggestions').hide();
+		}
+	});
+
+
+/*	Search auto-complete menu Functionality	*/
+	
+	
+function autocompleteMenuList() {
+	
+	if(getSearchValue() === null || getSearchValue() == ""){
+		
+	}else{
+		// Declare variables
+		var input, filter, ul, li, a, i;
+		input = document.getElementById('searchbar');
+		filter = input.value.toUpperCase();
+		ul = document.getElementById("search-suggestions");
+		li = ul.getElementsByTagName('li');
+
+		// Loop through all list items, and hide those who don't match the search query
+		for (i = 0; i < li.length; i++) {
+			a = li[i].getElementsByTagName("a")[0];
+			if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+				li[i].style.display = "";
+			} else {
+				li[i].style.display = "none";
+			}
+		}
+		
+	}
+	
+}
+	
+
+
+
+
 /*	SEARCH FUNCTIONALITY	*/
 
 
@@ -43,14 +94,31 @@
 			}
 		});
 		
+		
+		//Trigger 
+		input.addEventListener("keyup", function(event){
+			event.preventDefault();
+			$('#search-suggestions').show();
+			
+		});
+		
 	}//end window.onload
-
-
+	
+	
 
 	/*	Search button functionality	*/
 
 	//load search results website on button click
 	function search(){
-		onclick=window.location.href = './page_2.html?query=' + getSearchValue();;
+		onclick=window.location.href = './page_2.html?query=' + getSearchValue();
 	}
-
+	
+	
+	function selectSearchItem(link){
+		//onclick= document.getElementById("searchbar").value= link.innerHTML;
+		//search();	//remove search, uncomment, continue to add multi item search functionality
+		
+		onclick=window.location.href = './page_2.html?query=' + link.innerHTML;
+	}
+	
+	
